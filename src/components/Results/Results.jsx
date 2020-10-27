2f9b4d15842d045d21b2f92c5bb5d4998cd14d3d
Stella <stella.dmt@gmail.com>
2020-10-27 18:39:27 +0100

    resolved conflits + created the base of trip cards

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../../assets/css/global.css';
 
 const Results = ({ location }) => {
   const filterOne = location.query.filter;
   return (
     <div>
-      {Object.keys(filterOne).map(
-        (indexTravel) => filterOne[indexTravel].title
-      )}
+      {Object.keys(filterOne).map((indexTravel) => (
+        <div>
+          <div>carrousel</div>
+          <div>{filterOne[indexTravel].title}</div>
+          <div>{filterOne[indexTravel].country}</div>
+          <div>{filterOne[indexTravel].price}/pers</div>
+          <div>la journée</div>
+        </div>
+      ))}
     </div>
   );
 };
