cd99653320711b39e00b4bc51c3ad8d4ee41d637
Stella <stella.dmt@gmail.com>
2020-10-27 20:51:21 +0100

    added sensation icon

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import pin from '../../assets/images/location-pin.png';
+import fire from '../../assets/images/fire.png';
 import '../../assets/css/global.css';
 
 const Results = ({ location }) => {
@@ -9,18 +10,22 @@ const Results = ({ location }) => {
     <div className="results-list">
       {Object.keys(filterOne).map((indexTravel) => (
         <div className="trip-card">
-          <div className="trip-card-carrousel">carrousel</div>
-          <div className="trip-caption">
-            <div className="trip-info">
-              <div>{filterOne[indexTravel].title}</div>
-              <div className="trip-info-country">
-                <img className="pin" src={pin} alt="pin" />
-                {filterOne[indexTravel].country}
-              </div>
+          <div className="trip-container">
+            <div className="trip-card-carrousel">
+              <img className="trip-sensation-meter" src={fire} alt="fire" />
             </div>
-            <div className="trip-price">
-              <div>{filterOne[indexTravel].price}€/pers</div>
-              <div>la journée</div>
+            <div className="trip-caption">
+              <div className="trip-info">
+                <div>{filterOne[indexTravel].title}</div>
+                <div className="trip-info-country">
+                  <img className="pin" src={pin} alt="pin" />
+                  {filterOne[indexTravel].country}
+                </div>
+              </div>
+              <div className="trip-price">
+                <div>{filterOne[indexTravel].price}€/pers</div>
+                <div>la journée</div>
+              </div>
             </div>
           </div>
         </div>
