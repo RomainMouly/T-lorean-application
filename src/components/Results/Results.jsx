0a4d0c2fdb980365276756c79124afe3360336b1
Stella <stella.dmt@gmail.com>
2020-10-27 21:28:19 +0100

    added number of results

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -7,29 +7,35 @@ import '../../assets/css/global.css';
 const Results = ({ location }) => {
   const filterOne = location.query.filter;
   return (
-    <div className="results-list">
-      {Object.keys(filterOne).map((indexTravel) => (
-        <div className="trip-card">
-          <div className="trip-container">
-            <div className="trip-card-carrousel">
-              <img className="trip-sensation-meter" src={fire} alt="fire" />
-            </div>
-            <div className="trip-caption">
-              <div className="trip-info">
-                <div>{filterOne[indexTravel].title}</div>
-                <div className="trip-info-country">
-                  <img className="pin" src={pin} alt="pin" />
-                  {filterOne[indexTravel].country}
-                </div>
+    <div className="results">
+      <div className="results-number">
+        {Object.keys(filterOne).length} résultat
+        {Object.keys(filterOne).length > 1 && 's'}
+      </div>
+      <div className="results-list">
+        {Object.keys(filterOne).map((indexTravel) => (
+          <div className="trip-card">
+            <div className="trip-container">
+              <div className="trip-card-carrousel">
+                <img className="trip-sensation-meter" src={fire} alt="fire" />
               </div>
-              <div className="trip-price">
-                <div>{filterOne[indexTravel].price}€/pers</div>
-                <div>la journée</div>
+              <div className="trip-caption">
+                <div className="trip-info">
+                  <div>{filterOne[indexTravel].title}</div>
+                  <div className="trip-info-country">
+                    <img className="pin" src={pin} alt="pin" />
+                    {filterOne[indexTravel].country}
+                  </div>
+                </div>
+                <div className="trip-price">
+                  <div>{filterOne[indexTravel].price}€/pers</div>
+                  <div>la journée</div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
