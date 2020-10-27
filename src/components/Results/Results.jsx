21659506fe2d84609cf0bb39fa561cede93b3856
Stella <stella.dmt@gmail.com>
2020-10-27 19:33:49 +0100

    drafted trip cards

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import pin from '../../assets/images/location-pin.png';
 import '../../assets/css/global.css';
 
 const Results = ({ location }) => {
   const filterOne = location.query.filter;
   return (
-    <div>
+    <div className="results-list">
       {Object.keys(filterOne).map((indexTravel) => (
-        <div>
-          <div>carrousel</div>
-          <div>{filterOne[indexTravel].title}</div>
-          <div>{filterOne[indexTravel].country}</div>
-          <div>{filterOne[indexTravel].price}/pers</div>
-          <div>la journée</div>
+        <div className="trip-card">
+          <div className="trip-card-carrousel">carrousel</div>
+          <div className="trip-caption">
+            <div className="trip-info">
+              <div>{filterOne[indexTravel].title}</div>
+              <div className="trip-info-country">
+                <img className="pin" src={pin} alt="pin" />
+                {filterOne[indexTravel].country}
+              </div>
+            </div>
+            <div className="trip-price">
+              <div>{filterOne[indexTravel].price}€/pers</div>
+              <div>la journée</div>
+            </div>
+          </div>
         </div>
       ))}
     </div>
