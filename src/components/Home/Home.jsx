4961784ffc3b7cb0b92952745cc7e47338484cca
RomainMouly <romainmouly@gmail.com>
2020-10-28 14:57:18 +0100

    searchBar hooks

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,91 +1,77 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import FilterTravel from '../componentsGlobal/FilterTravel';
 import '../../assets/css/Home/Home.css';
 import Welcome from './Welcome';
 import SearchBar from './SearchBar';
 
-class Home extends Component {
-  constructor() {
-    super();
-    this.state = {
-      travels: [],
-    };
-  }
+const Home = () => {
+  const [travels, setTravels] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     axios
       .get('https://api-airbnb-node.herokuapp.com/api/travels')
-      .then((response) => this.setState({ travels: response.data }));
-  }
+      .then((response) => setTravels(response.data));
+  }, []);
 
-  render() {
-    const { travels } = this.state;
-    const filterTravelSensationOne = travels.filter(
-      (travel) => travel.level === 1
-    );
-    const filterTravelSensationTwo = travels.filter(
-      (travel) => travel.level === 2
-    );
-    const filterTravelSensationThree = travels.filter(
-      (travel) => travel.level === 3
-    );
-    const filterTravelEraPrehistoire = travels.filter(
-      (travel) => travel.era === 'Préhistoire'
-    );
-    const filterTravelEraAntiquite = travels.filter(
-      (travel) => travel.era === 'Antiquité'
-    );
-    const filterTravelEraMoyenAge = travels.filter(
-      (travel) => travel.era === 'Moyen-Âge'
-    );
-    const filterTravelEraRenaissance = travels.filter(
-      (travel) => travel.era === 'Renaissance'
-    );
-    const filterTravelEraTempsModernes = travels.filter(
-      (travel) => travel.era === 'Temps modernes'
-    );
-    const filterTravelEraFutur = travels.filter(
-      (travel) => travel.era === 'Futur'
-    );
+  const filterTravelSensationOne = travels.filter(
+    (travel) => travel.level === 1
+  );
+  const filterTravelSensationTwo = travels.filter(
+    (travel) => travel.level === 2
+  );
+  const filterTravelSensationThree = travels.filter(
+    (travel) => travel.level === 3
+  );
+  const filterTravelEraPrehistoire = travels.filter(
+    (travel) => travel.era === 'Préhistoire'
+  );
+  const filterTravelEraAntiquite = travels.filter(
+    (travel) => travel.era === 'Antiquité'
+  );
+  const filterTravelEraMoyenAge = travels.filter(
+    (travel) => travel.era === 'Moyen-Âge'
+  );
+  const filterTravelEraRenaissance = travels.filter(
+    (travel) => travel.era === 'Renaissance'
+  );
+  const filterTravelEraTempsModernes = travels.filter(
+    (travel) => travel.era === 'Temps modernes'
+  );
+  const filterTravelEraFutur = travels.filter(
+    (travel) => travel.era === 'Futur'
+  );
 
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          <Welcome />
+        <Welcome />
+      </div>
+      <div>
+        <SearchBar />
+      </div>
+      <div className="home-filters">
+        <h2>Époques</h2>
+        <div className="home-filter-era">
+          <FilterTravel data={filterTravelEraPrehistoire} type="Préhistoire" />
+          <FilterTravel data={filterTravelEraAntiquite} type="Antiquité" />
+          <FilterTravel data={filterTravelEraMoyenAge} type="Moyen-Âge" />
+          <FilterTravel data={filterTravelEraRenaissance} type="Renaissance" />
+          <FilterTravel
+            data={filterTravelEraTempsModernes}
+            type="Temps modernes"
+          />
+          <FilterTravel data={filterTravelEraFutur} type="Futur" />
         </div>
-        <div>
-          <SearchBar />
-        </div>
-        <div className="home-filters">
-          <h2>Époques</h2>
-          <div className="home-filter-era">
-            <FilterTravel
-              data={filterTravelEraPrehistoire}
-              type="Préhistoire"
-            />
-            <FilterTravel data={filterTravelEraAntiquite} type="Antiquité" />
-            <FilterTravel data={filterTravelEraMoyenAge} type="Moyen-Âge" />
-            <FilterTravel
-              data={filterTravelEraRenaissance}
-              type="Renaissance"
-            />
-            <FilterTravel
-              data={filterTravelEraTempsModernes}
-              type="Temps modernes"
-            />
-            <FilterTravel data={filterTravelEraFutur} type="Futur" />
-          </div>
-          <h2>Sensations</h2>
-          <div className="home-filter-level">
-            <FilterTravel data={filterTravelSensationOne} type="Calme" />
-            <FilterTravel data={filterTravelSensationTwo} type="Aventure" />
-            <FilterTravel data={filterTravelSensationThree} type="Extrême" />
-          </div>
+        <h2>Sensations</h2>
+        <div className="home-filter-level">
+          <FilterTravel data={filterTravelSensationOne} type="Calme" />
+          <FilterTravel data={filterTravelSensationTwo} type="Aventure" />
+          <FilterTravel data={filterTravelSensationThree} type="Extrême" />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Home;
--- src/components/Home/SearchBar.jsx
+++ src/components/Home/SearchBar.jsx
@@ -1,13 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const SearchBar = () => {
   const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('https://api-airbnb-node.herokuapp.com/api/travels')
+      .then((response) => setQuery(response.data.title));
+  }, []);
 
   return (
     <form>
       <input
         type="text"
-        placeholder="Search for..."
+        placeholder="Entrer le nom de l'expérience"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
       />
