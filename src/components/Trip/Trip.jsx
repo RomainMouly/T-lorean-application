7a3fe76d7169c15790b17ace5ca2293c8aef5a09
SalemDrenay <salemdrenay@gmail.com>
2020-11-04 17:10:38 +0100

    cleaned the axios calls, added mentions that will be displayed on every TRIP page, displayed a bar with country and price under the title

--- src/components/Trip/Trip.jsx
+++ src/components/Trip/Trip.jsx
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import '../../assets/css/trip.css';
 import CarouselDetail from './CarouselDetail';
 import Description from './Description';
 import BookingButton from './BookingButton';
-import '../../assets/css/trip.css';
 
-const Trip = ({ description, match }) => {
+const Trip = ({ match }) => {
+  const { id } = match.params;
+  const [travel, setTravel] = useState({});
+  console.log(process.env.REACT_APP_BACK);
+
+  useEffect(() => {
+    axios
+      .get(`https://api-airbnb-node.herokuapp.com/api/travels/${id}`)
+      .then((result) => result.data[0])
+      .then((data) => setTravel(data));
+  }, [id]);
+  console.log(travel);
   return (
     <div className="trip">
       {' '}
       <CarouselDetail />
-      <Description description={description} match={match} />
-      <BookingButton />
+      <Description
+        travelTitle={travel.title}
+        travelDescription={travel.description}
+        travelPrice={travel.price}
+        travelCountry={travel.country}
+      />
+      <BookingButton travelId={travel.id} />
     </div>
   );
+};
+
+Description.propTypes = {
+  match: PropTypes.shape({}).isRequired,
 };
 
 export default Trip;
