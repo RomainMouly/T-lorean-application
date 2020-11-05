cb8eb68e8706479edb13b731921cda7ae3641dff
Stella <stella.dmt@gmail.com>
2020-11-05 11:46:06 +0100

    fixed all path

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import '../../assets/css/global.css';
-import '../../assets/css/Results/Results.css';
+import axios from 'axios';
+
 import ResultNumber from './ResultNumber';
 import TripCard from './TripCard';
 
-const Results = ({ location }) => {
-  const filterOne = location.query.filter;
+import '../../assets/css/global.css';
+import '../../assets/css/Results/Results.css';
+
+const Results = ({ match }) => {
+  const { type, name } = match.params;
+  const [travels, setTravels] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://api-airbnb-node.herokuapp.com/api/travels')
+      .then((result) => result.data)
+      .then((data) => handleType(data));
+  }, []);
+
+  const handleType = (data) => {
+    if (name === 'era') {
+      setTravels(data.filter((travel) => travel.era === type));
+    }
+
+    if (name === '1' || (name === '2') | (name === '3')) {
+      setTravels(data.filter((travel) => travel.level === Number(name)));
+    }
+  };
+
   return (
     <div className="results">
-      <ResultNumber filterList={filterOne} />
+      <ResultNumber filterList={travels} />
       <div className="results-list">
-        {Object.keys(filterOne).map((indexTravel) => (
-            <Link className="travel-card-link" to={{ pathname: `/Trip/${filterOne[indexTravel].id}` }}>
-            <TripCard filteredTrips={filterOne} indexTravel={indexTravel} />
-          </Link>
-        ))}
+        {travels &&
+          travels.map((travel) => (
+            <Link
+              className="travel-card-link"
+              to={{ pathname: `/Trip/${travel.id}` }}
+            >
+              <TripCard filteredTrips={travel} />
+            </Link>
+          ))}
       </div>
     </div>
   );
