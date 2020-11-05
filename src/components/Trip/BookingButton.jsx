61695df25f1e17a93b2eca8eca77ef1781018ee3
RomainMouly <romainmouly@gmail.com>
2020-11-05 09:46:47 +0100

    resolve

--- src/components/Trip/BookingButton.jsx
+++ src/components/Trip/BookingButton.jsx
@@ -1,9 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-
-const BookingButton = () => {
-  return <button id="searchbarButton">Réserver</button>;
-=======
 import { Link } from 'react-router-dom';
 
 const BookingButton = ({ travelId }) => {
@@ -12,7 +7,6 @@ const BookingButton = ({ travelId }) => {
       Reserver
     </Link>
   );
->>>>>>> 5af416b43c71109a67c1ce69702e347b55b7b236
 };
 
 export default BookingButton;
