dc3dbd6b3feb7c7693dfb876169e1cc0ac88e149
Stella <stella.dmt@gmail.com>
2020-11-05 12:26:35 +0100

    finished sensation icon

--- src/components/Results/SensationMeter.jsx
+++ src/components/Results/SensationMeter.jsx
@@ -2,31 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../../assets/css/global.css';
 import '../../assets/css/Results/SensationMeter.css';
-import mountain from '../../assets/images/mountain.png';
 import flash from '../../assets/images/flash.png';
-import compass from '../../assets/images/compass.png';
 
 const SensationMeter = ({ filteredTripsLevel }) => {
   switch (filteredTripsLevel) {
     case 2:
       return (
         <div className="trip-sensation-meter">
-          <img src={mountain} alt="mountain" />
-          <img src={mountain} alt="mountain" />
+          <img src={flash} alt="flash" />
+          <img src={flash} alt="flash" />
         </div>
       );
     case 3:
       return (
         <div className="trip-sensation-meter">
-          <img src={mountain} alt="mountain" />
-          <img src={mountain} alt="mountain" />
-          <img src={mountain} alt="mountain" />
+          <img src={flash} alt="flash" />
+          <img src={flash} alt="flash" />
+          <img src={flash} alt="flash" />
         </div>
       );
     default:
       return (
         <div className="trip-sensation-meter">
-          <img src={mountain} alt="mountain" />
+          <img src={flash} alt="flash" />
         </div>
       );
   }
