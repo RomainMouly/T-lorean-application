e60c0399b2f310972f787921351ef64052c885ec
RomainMouly <romainmouly@gmail.com>
2020-11-06 09:42:57 +0100

    image trip

--- src/components/Trip/CarouselDetail.jsx
+++ src/components/Trip/CarouselDetail.jsx
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-/*import SensationMeter from '../Results/SensationMeter';*/
 
-const CarouselDetail = ({ filteredTrips, indexTravel }) => {
+const CarouselDetail = ({ pictures }) => {
+  console.log(pictures[0]);
   return (
     <div className="carousel-box">
-      {/*<SensationMeter filteredTripsLevel={filteredTrips[indexTravel].level} />*/}
+      {/* {pictures.map((picture) => (
+        <img src={picture.url} alt={picture.id} />
+      ))} pour avoir toutes les images avec le carousel, sinon pour une seule image voir ci dessous
+      on peut egalement enlever pictures*/}
+      <img src={pictures && pictures[1] && pictures[1].url} />{' '}
+      {/*besoin de faire ca car les photos api mettent du temps à arriver
+      du coup : on luii dit: si tu as une pictures et que tu as picture[1] tu affiches pictures[1].url
+      si tu as une image ou pas tu m'affiche ou pas l'url de l'image
+      obligé de faire ca pour une seule image dès que tu as du axios*/}
     </div>
   );
 };
