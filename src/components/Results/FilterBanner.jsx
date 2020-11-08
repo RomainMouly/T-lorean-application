48449e2d7f5eab4c8f0dc3d55a64b522948623b8
Stella <stella.dmt@gmail.com>
2020-11-08 08:12:54 +0100

    refacto FilterBanner.jsx

--- src/components/Results/FilterBanner.jsx
+++ src/components/Results/FilterBanner.jsx
@@ -13,262 +13,120 @@ import level2 from '../../assets/images/filters/level2.jpeg';
 import level3 from '../../assets/images/filters/level3.jpeg';
 
 const FilterBanner = (props) => {
-  const { filterName } = props;
+  const { filterType, filterName, handleSubfiltering } = props;
+  let filterNameTitle = '';
+  let filterNameSource = '';
+
   switch (filterName) {
     case 'Préhistoire':
-      return (
-        <div>
-          <div className="nav-filter">
-            <h2>{filterName}</h2>
-          </div>
-          <img className="filter-banner" src={era1} alt={filterName} />
-          <h3 className="subfiltering">Trier par sensations</h3>
-          <div className="subfilter-list">
-            <div className="subfilter-card">
-              <img className="subfilter" src={level1} alt="Détente" />
-              <h4>Détente</h4>
-            </div>
-            <div className="subfilter-card">
-              <img className="subfilter" src={level2} alt="Aventure" />
-              <h4>Aventure</h4>
-            </div>
-            <div className="subfilter-card">
-              <img className="subfilter" src={level3} alt="Extrême" />
-              <h4>Extrême</h4>
-            </div>
-          </div>
-        </div>
-      );
+      filterNameTitle = 'Préhistoire';
+      filterNameSource = `${era1}`;
+      break;
     case 'Antiquité':
-      return (
-        <div>
-          <div className="nav-filter">
-            <h2>{filterName}</h2>
-          </div>
-          <img className="filter-banner" src={era2} alt={filterName} />
-          <h3 className="subfiltering">Trier par sensations</h3>
-          <div className="subfilter-list">
-            <div className="subfilter-card">
-              <img className="subfilter" src={level1} alt="Détente" />
-              <h4>Détente</h4>
-            </div>
-            <div className="subfilter-card">
-              <img className="subfilter" src={level2} alt="Aventure" />
-              <h4>Aventure</h4>
-            </div>
-            <div className="subfilter-card">
-              <img className="subfilter" src={level3} alt="Extrême" />
-              <h4>Extrême</h4>
-            </div>
-          </div>
-        </div>
-      );
+      filterNameTitle = 'Antiquité';
+      filterNameSource = `${era2}`;
+      break;
     case 'Moyen-Âge':
-      return (
-        <div>
-          <div className="nav-filter">
-            <h2>{filterName}</h2>
-          </div>
-          <img className="filter-banner" src={era3} alt={filterName} />
-          <h3 className="subfiltering">Trier par sensations</h3>
-          <div className="subfilter-list">
-            <div className="subfilter-card">
-              <img className="subfilter" src={level1} alt="Détente" />
-              <h4>Détente</h4>
-            </div>
-            <div className="subfilter-card">
-              <img className="subfilter" src={level2} alt="Aventure" />
-              <h4>Aventure</h4>
-            </div>
-            <div className="subfilter-card">
-              <img className="subfilter" src={level3} alt="Extrême" />
-              <h4>Extrême</h4>
-            </div>
-          </div>
-        </div>
-      );
+      filterNameTitle = 'Moyen-Âge';
+      filterNameSource = `${era3}`;
+      break;
     case 'Renaissance':
-      return (
-        <div>
-          <div className="nav-filter">
-            <h2>{filterName}</h2>
-          </div>
-          <img className="filter-banner" src={era4} alt={filterName} />
-          <h3 className="subfiltering">Trier par sensations</h3>
-          <div className="subfilter-list">
-            <div className="subfilter-card">
-              <img className="subfilter" src={level1} alt="Détente" />
-              <h4>Détente</h4>
-            </div>
-            <div className="subfilter-card">
-              <img className="subfilter" src={level2} alt="Aventure" />
-              <h4>Aventure</h4>
-            </div>
-            <div className="subfilter-card">
-              <img className="subfilter" src={level3} alt="Extrême" />
-              <h4>Extrême</h4>
-            </div>
-          </div>
-        </div>
-      );
+      filterNameTitle = 'Renaissance';
+      filterNameSource = `${era4}`;
+      break;
     case 'Temps modernes':
-      return (
-        <div>
-          <div className="nav-filter">
-            <h2>{filterName}</h2>
-          </div>
-          <img className="filter-banner" src={era5} alt={filterName} />
-          <h3 className="subfiltering">Trier par sensations</h3>
-          <div className="subfilter-list">
-            <div className="subfilter-card">
-              <img className="subfilter" src={level1} alt="Détente" />
-              <h4>Détente</h4>
-            </div>
-            <div className="subfilter-card">
-              <img className="subfilter" src={level2} alt="Aventure" />
-              <h4>Aventure</h4>
-            </div>
-            <div className="subfilter-card">
-              <img className="subfilter" src={level3} alt="Extrême" />
-              <h4>Extrême</h4>
-            </div>
-          </div>
-        </div>
-      );
+      filterNameTitle = 'Temps modernes';
+      filterNameSource = `${era5}`;
+      break;
     case 'Futur':
-      return (
-        <div>
-          <div className="nav-filter">
-            <h2>{filterName}</h2>
-          </div>
-          <img className="filter-banner" src={era6} alt={filterName} />
-          <h3 className="subfiltering">Trier par sensations</h3>
-          <div className="subfilter-list">
-            <div className="subfilter-card">
-              <img className="subfilter" src={level1} alt="Détente" />
-              <h4>Détente</h4>
-            </div>
-            <div className="subfilter-card">
-              <img className="subfilter" src={level2} alt="Aventure" />
-              <h4>Aventure</h4>
-            </div>
-            <div className="subfilter-card">
-              <img className="subfilter" src={level3} alt="Extrême" />
-              <h4>Extrême</h4>
-            </div>
-          </div>
-        </div>
-      );
+      filterNameTitle = 'Futur';
+      filterNameSource = `${era6}`;
+      break;
     case 'Détente':
-      return (
-        <div>
-          <div className="nav-filter">
-            <h2>{filterName}</h2>
-          </div>
-          <img className="filter-banner" src={level1} alt={filterName} />
-          <h3 className="subfiltering">Trier par époques</h3>
-          <div className="subfilter-list">
-            <div className="subfilter-card">
-              <img className="subfilter" src={era1} alt="Préhistoire" />
-              <h4>Préhistoire</h4>
-            </div>
-            <div className="subfilter-card">
-              <img className="subfilter" src={era2} alt="Antiquité" />
-              <h4>Antiquité</h4>
-            </div>
-            <div className="subfilter-card">
-              <img className="subfilter" src={era3} alt="Moyen-Âge" />
-              <h4>Moyen-Âge</h4>
-            </div>
-            <div className="subfilter-card">
-              <img className="subfilter" src={era4} alt="Renaissance" />
-              <h4>Renaissance</h4>
-            </div>
-            <div className="subfilter-card">
-              <img className="subfilter" src={era5} alt="Temps modernes" />
-              <h4>Temps modernes</h4>
-            </div>
-            <div className="subfilter-card">
-              <img className="subfilter" src={era6} alt="Futur" />
-              <h4>Futur</h4>
-            </div>
-          </div>
-        </div>
-      );
+      filterNameTitle = 'Détente';
+      filterNameSource = `${level1}`;
+      break;
     case 'Aventure':
-      return (
-        <div>
-          <div className="nav-filter">
-            <h2>{filterName}</h2>
-          </div>
-          <img className="filter-banner" src={level2} alt={filterName} />
-          <h3 className="subfiltering">Trier par époques</h3>
-          <div className="subfilter-list">
-            <div className="subfilter-card">
-              <img className="subfilter" src={era1} alt="Préhistoire" />
-              <h4>Préhistoire</h4>
-            </div>
-            <div className="subfilter-card">
-              <img className="subfilter" src={era2} alt="Antiquité" />
-              <h4>Antiquité</h4>
-            </div>
-            <div className="subfilter-card">
-              <img className="subfilter" src={era3} alt="Moyen-Âge" />
-              <h4>Moyen-Âge</h4>
-            </div>
-            <div className="subfilter-card">
-              <img className="subfilter" src={era4} alt="Renaissance" />
-              <h4>Renaissance</h4>
-            </div>
-            <div className="subfilter-card">
-              <img className="subfilter" src={era5} alt="Temps modernes" />
-              <h4>Temps modernes</h4>
-            </div>
-            <div className="subfilter-card">
-              <img className="subfilter" src={era6} alt="Futur" />
-              <h4>Futur</h4>
-            </div>
-          </div>
-        </div>
-      );
+      filterNameTitle = 'Aventure';
+      filterNameSource = `${level2}`;
+      break;
     case 'Extrême':
-      return (
-        <div>
-          <div className="nav-filter">
-            <h2>{filterName}</h2>
+      filterNameTitle = 'Extrême';
+      filterNameSource = `${level3}`;
+      break;
+    default:
+      filterNameTitle = 'Try again!';
+      filterNameSource = `${era1}`;
+  }
+
+  if (filterType === 'era') {
+    return (
+      <div>
+        <div className="nav-filter">
+          <h2>{filterNameTitle}</h2>
+        </div>
+        <img
+          className="filter-banner"
+          src={filterNameSource}
+          alt={filterNameTitle}
+        />
+        <h3 className="subfiltering">Trier par sensations</h3>
+        <div className="subfilter-list">
+          <div className="subfilter-card" onClick={handleSubfiltering}>
+            <img className="subfilter" src={level1} alt="Détente" />
+            <h4>Détente</h4>
           </div>
-          <img className="filter-banner" src={level3} alt={filterName} />
-          <h3 className="subfiltering">Trier par époques</h3>
-          <div className="subfilter-list">
-            <div className="subfilter-card">
-              <img className="subfilter" src={era1} alt="Préhistoire" />
-              <h4>Préhistoire</h4>
-            </div>
-            <div className="subfilter-card">
-              <img className="subfilter" src={era2} alt="Antiquité" />
-              <h4>Antiquité</h4>
-            </div>
-            <div className="subfilter-card">
-              <img className="subfilter" src={era3} alt="Moyen-Âge" />
-              <h4>Moyen-Âge</h4>
-            </div>
-            <div className="subfilter-card">
-              <img className="subfilter" src={era4} alt="Renaissance" />
-              <h4>Renaissance</h4>
-            </div>
-            <div className="subfilter-card">
-              <img className="subfilter" src={era5} alt="Temps modernes" />
-              <h4>Temps modernes</h4>
-            </div>
-            <div className="subfilter-card">
-              <img className="subfilter" src={era6} alt="Futur" />
-              <h4>Futur</h4>
-            </div>
+          <div className="subfilter-card" onClick={handleSubfiltering}>
+            <img className="subfilter" src={level2} alt="Aventure" />
+            <h4>Aventure</h4>
+          </div>
+          <div className="subfilter-card" onClick={handleSubfiltering}>
+            <img className="subfilter" src={level3} alt="Extrême" />
+            <h4>Extrême</h4>
           </div>
         </div>
-      );
-    default:
-      return <div>Try again!</div>;
+      </div>
+    );
+  } else if (filterType === '1' || filterType === '2' || filterType === '3') {
+    return (
+      <div>
+        <div className="nav-filter">
+          <h2>{filterNameTitle}</h2>
+        </div>
+        <img
+          className="filter-banner"
+          src={filterNameSource}
+          alt={filterNameTitle}
+        />
+        <h3 className="subfiltering">Trier par époques</h3>
+        <div className="subfilter-list">
+          <div className="subfilter-card" onClick={handleSubfiltering}>
+            <img className="subfilter" src={era1} alt="Préhistoire" />
+            <h4>Préhistoire</h4>
+          </div>
+          <div className="subfilter-card" onClick={handleSubfiltering}>
+            <img className="subfilter" src={era2} alt="Antiquité" />
+            <h4>Antiquité</h4>
+          </div>
+          <div className="subfilter-card" onClick={handleSubfiltering}>
+            <img className="subfilter" src={era3} alt="Moyen-Âge" />
+            <h4>Moyen-Âge</h4>
+          </div>
+          <div className="subfilter-card" onClick={handleSubfiltering}>
+            <img className="subfilter" src={era4} alt="Renaissance" />
+            <h4>Renaissance</h4>
+          </div>
+          <div className="subfilter-card" onClick={handleSubfiltering}>
+            <img className="subfilter" src={era5} alt="Temps modernes" />
+            <h4>Temps modernes</h4>
+          </div>
+          <div className="subfilter-card" onClick={handleSubfiltering}>
+            <img className="subfilter" src={era6} alt="Futur" />
+            <h4>Futur</h4>
+          </div>
+        </div>
+      </div>
+    );
   }
 };
 
