6ef945f93cf42214747776147ce81732c2321201
fdureux <fanny.dureux@gmail.com>
2020-11-11 14:40:16 +0100

    fix form

--- src/components/Booking/Form.jsx
+++ src/components/Booking/Form.jsx
@@ -1,101 +1,104 @@
-// import React, { useState } from 'react';
-// import axios from 'axios';
-// import '../../assets/css/form.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import '../../assets/css/form.css';
 
-// const Form = () => {
-//   const [form, setForm] = useState({ lastname: '', firstname: '', email: '' });
-//   const [validForm, setValidForm] = useState('');
-//   const [errorForm, setErrorForm] = useState('');
+const Form = () => {
+  const [form, setForm] = useState({ lastname: '', firstname: '', email: '' });
+  const [validForm, setValidForm] = useState('');
+  const [errorForm, setErrorForm] = useState('');
 
-//   const handleChange = (e) => {
-//     setForm({
-//       ...form,
-//       [e.target.name]: e.target.value,
-//     });
-//   };
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-//   const handleValidForm = () => {
-//     setValidForm(
-//       `Félicitation ${form.firstname}, votre réservation a bien été prise en compte. Préparez-vous pour l'aventure !`
-//     );
-//   };
+  const handleValidForm = () => {
+    setValidForm(
+      `Félicitation ${form.firstname}, votre réservation a bien été prise en compte. Préparez-vous pour l'aventure !`
+    );
+  };
 
-//   const handleErrorForm = (e) => {
-//     setErrorForm(
-//       `Erreur lors de votre réservation : ${e.message}, veuillez réessayer`
-//     );
-//   };
+  const handleErrorForm = (e) => {
+    setErrorForm(
+      `Erreur lors de votre réservation : ${e.message}, veuillez réessayer`
+    );
+  };
 
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     axios
-//       .post('https://api-airbnb-node.herokuapp.com/api/users', form)
-//       .then((res) => res.data)
-//       .then((data) => {
-//         handleValidForm();
-//       })
-//       .catch(() => {
-//         handleErrorForm(e);
-//       });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post('https://api-airbnb-node.herokuapp.com/api/users', form)
+      .then(() => handleValidForm())
+      .catch((err) => {
+        handleErrorForm(err);
+      });
 
-//     setForm({ ...form, lastname: '', firstname: '', email: '' });
-//   };
+    setForm({ ...form, lastname: '', firstname: '', email: '' });
+  };
 
-//   return (
-//     <div className="form">
-//       <h1>Réserver votre voyage</h1>
+  return (
+    <div className="form">
+      <h1>Réserver votre voyage</h1>
 
-//       <form onSubmit={handleSubmit}>
-//         <fieldset>
-//           <legend>Vos Informations</legend>
-//           <div className="form-data">
-//             <label htmlFor="lastname">Nom</label>
-//             <input
-//               type="text"
-//               id="lastname"
-//               name="lastname"
-//               onChange={handleChange}
-//               value={form.lastname}
-//               required
-//             />
-//           </div>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <legend>Vos Informations</legend>
+          <div className="form-data">
+            <label htmlFor="lastname">
+              <input
+                type="text"
+                id="lastname"
+                name="lastname"
+                placeholder="Nom"
+                onChange={handleChange}
+                value={form.lastname}
+                required
+              />
+            </label>
+          </div>
 
-//           <div className="form-data">
-//             <label htmlFor="firstname">Prénom</label>
-//             <input
-//               type="text"
-//               id="firstname"
-//               name="firstname"
-//               onChange={handleChange}
-//               value={form.firstname}
-//               required
-//             />
-//           </div>
+          <div className="form-data">
+            <label htmlFor="firstname">
+              <input
+                type="text"
+                id="firstname"
+                name="firstname"
+                placeholder="Prénom"
+                onChange={handleChange}
+                value={form.firstname}
+                required
+              />
+            </label>
+          </div>
 
-//           <div className="form-data">
-//             <label htmlFor="email">E-mail</label>
-//             <input
-//               type="email"
-//               id="email"
-//               name="email"
-//               onChange={handleChange}
-//               value={form.email}
-//               required
-//             />
-//           </div>
-//           <div className="form-data">
-//             <button type="button" className="colorButton form-button">
-//               Réserver
-//             </button>
-//           </div>
-//           <p>
-//             {validForm}
-//             {errorForm}
-//           </p>
-//         </fieldset>
-//       </form>
-//     </div>
-//   );
-// };
+          <div className="form-data">
+            <label htmlFor="email">
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="e-mail"
+                onChange={handleChange}
+                value={form.email}
+                required
+              />
+            </label>
+          </div>
+          <div className="form-data">
+            <button type="submit" className="colorButton form-button">
+              Réserver
+            </button>
+          </div>
+          <p>
+            {validForm}
+            {errorForm}
+          </p>
+        </fieldset>
+      </form>
+    </div>
+  );
+};
 
-// export default Form;
+export default Form;
